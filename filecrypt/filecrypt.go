c14c218df4bf77bc4bf1722113609ee6ed3a3d84
Sahil <sahimake1773@gmail.com>
2023-10-22 22:07:43 +0530

    decrypt function done

--- filecrypt/filecrypt.go
+++ filecrypt/filecrypt.go
@@ -5,6 +5,7 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha1"
+	"encoding/hex"
 	"io"
 	"os"
 
@@ -64,5 +65,55 @@ func Encrypt(source string, password []byte) {
 
 }
 func Decrypt(source string, password []byte) {
+	if _, err := os.Stat(source); os.IsNotExist(err) {
+		panic(err.Error())
+	}
+
+	srcFile, err := os.Open(source)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer srcFile.Close()
+
+	cipherText, err := io.ReadAll(srcFile)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	key := password
+
+	salt := cipherText[len(cipherText)-12:]
+	str := hex.EncodeToString(salt)
+	nonce, err := hex.DecodeString(str)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	dk := pbkdf2.Key(key, nonce, 4096, 32, sha1.New)
+	block, err := aes.NewCipher(dk)
+	if err != nil {
+		panic(err.Error())
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	plainText, err := aesgcm.Open(nil, nonce, cipherText[:len(cipherText)-12], nil)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	dstFile, err := os.Create(source)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer dstFile.Close()
+
+	_, err = dstFile.Write(plainText)
+	if err != nil {
+		panic(err.Error())
+	}
 
 }
